4b78060826d556633f88542fa0617de58f11ad3d
pvova21 <podrezov08@rambler.ru>
2022-12-12 13:26:57 +0300

    fix webpack commit

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   output: {
     filename: 'app.bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
